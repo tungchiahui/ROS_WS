c52ce7f1dd849cb0def2c88411ca543a6b7273d2
tungchiahui <tungchiahui@gmail.com>
2023-12-31 00:04:15 +0800

    URDF基础实现更新

--- 4.ws02_tools/install/local_setup.sh
+++ 4.ws02_tools/install/local_setup.sh
@@ -6,7 +6,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_sh_COLCON_CURRENT_PREFIX="/home/tungchiahui/mysource/ros2src/4.ws02_tools/install"
+_colcon_prefix_sh_COLCON_CURRENT_PREFIX="/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/install"
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   if [ ! -d "$_colcon_prefix_sh_COLCON_CURRENT_PREFIX" ]; then
     echo "The build time path \"$_colcon_prefix_sh_COLCON_CURRENT_PREFIX\" doesn't exist. Either source a script for a different shell or set the environment variable \"COLCON_CURRENT_PREFIX\" explicitly." 1>&2
