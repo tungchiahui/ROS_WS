491fdf7a726a878a96637a12a3143a2779af2cda
tungchiahui <tungchiahui@gmail.com>
2024-12-29 14:25:00 +0800

    rosbag2

--- 4.ws02_tools/build/cpp06_urdf/cmake_install.cmake
+++ 4.ws02_tools/build/cpp06_urdf/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/tungchiahui/user/mysource/ros2src/4.ws02_tools/src/cpp06_urdf
+# Install script for directory: /home/tungchiahui/source/ROS2_WS/4.ws02_tools/src/cpp06_urdf
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/install/cpp06_urdf")
+  set(CMAKE_INSTALL_PREFIX "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/install/cpp06_urdf")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -42,72 +42,72 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE DIRECTORY FILES
-    "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/src/cpp06_urdf/launch"
-    "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/src/cpp06_urdf/urdf"
-    "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/src/cpp06_urdf/rviz"
-    "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/src/cpp06_urdf/meshes"
+    "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/src/cpp06_urdf/launch"
+    "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/src/cpp06_urdf/urdf"
+    "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/src/cpp06_urdf/rviz"
+    "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/src/cpp06_urdf/meshes"
     )
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/cpp06_urdf")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/cpp06_urdf")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/parent_prefix_path" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_index/share/ament_index/resource_index/parent_prefix_path/cpp06_urdf")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/parent_prefix_path" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_index/share/ament_index/resource_index/parent_prefix_path/cpp06_urdf")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/ament_prefix_path.sh")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf/environment" TYPE FILE FILES "/opt/ros/jazzy/share/ament_cmake_core/cmake/environment_hooks/environment/ament_prefix_path.sh")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf/environment" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/ament_prefix_path.dsv")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf/environment" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/ament_prefix_path.dsv")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/path.sh")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf/environment" TYPE FILE FILES "/opt/ros/jazzy/share/ament_cmake_core/cmake/environment_hooks/environment/path.sh")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf/environment" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/path.dsv")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf/environment" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/path.dsv")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/local_setup.bash")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/local_setup.bash")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/local_setup.sh")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/local_setup.sh")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/local_setup.zsh")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/local_setup.zsh")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/local_setup.dsv")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/local_setup.dsv")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/package.dsv")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_environment_hooks/package.dsv")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/packages" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_index/share/ament_index/resource_index/packages/cpp06_urdf")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/packages" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_index/share/ament_index/resource_index/packages/cpp06_urdf")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf/cmake" TYPE FILE FILES
-    "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_core/cpp06_urdfConfig.cmake"
-    "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/ament_cmake_core/cpp06_urdfConfig-version.cmake"
+    "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_core/cpp06_urdfConfig.cmake"
+    "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/ament_cmake_core/cpp06_urdfConfig-version.cmake"
     )
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/src/cpp06_urdf/package.xml")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cpp06_urdf" TYPE FILE FILES "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/src/cpp06_urdf/package.xml")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -118,5 +118,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/build/cpp06_urdf/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/tungchiahui/source/ROS2_WS/4.ws02_tools/build/cpp06_urdf/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- 4.ws02_tools/install/cpp01_launch/share/cpp01_launch/local_setup.sh
+++ 4.ws02_tools/install/cpp01_launch/share/cpp01_launch/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/tungchiahui/user/mysource/ros2src/4.ws02_tools/install/cpp01_launch"}
+: ${AMENT_CURRENT_PREFIX:="/home/tungchiahui/source/ROS2_WS/4.ws02_tools/install/cpp01_launch"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
