be748e11d68ed6bd2a77cd35f0be9cac3f309b36
tungchiahui <tungchiahui@gmail.com>
2025-01-08 15:14:59 +0000

    update nav2_urdf

--- 6.ws_simulations/build/Stage/worlds/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tungchiahui/UserFloder/mysource/ROS/ROS2_WS/6.ws_simulations/src/Stage/worlds
-# Build directory: /home/tungchiahui/UserFloder/mysource/ROS/ROS2_WS/6.ws_simulations/build/Stage/worlds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("benchmark")
-subdirs("bitmaps")
-subdirs("wifi")
