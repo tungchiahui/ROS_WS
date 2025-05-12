fe055fe637b93719d7169064e4ef09b4a7b7ad36
tungchiahui <tungchiahui@gmail.com>
2025-05-12 12:35:38 +0000

    add gazebo classic

---
+++ ROS2_WS/6.ws_simulations/src/fishbot_description/launch/gazebo.launch.py
@@ -0,0 +1,43 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+# 封装终端指令相关类
+from launch.actions import ExecuteProcess
+# from launch.substitutions import FindExecutable
+# 参数声明与获取
+# from launch.actions import DeclareLaunchArgument
+# from launch.substitutions import LaunchConfiguration
+# 文件包含相关
+# from launch.actions import IncludeLaunchDescription
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+# 分组相关
+# from launch_ros.actions import PushRosNamespace
+# from launch.actions import GroupAction
+# 事件相关
+# from launch.event_handlers import OnProcessStart,OnProcessExit
+# from launch.actions import ExecuteProcess,RegisterEventHandler,LogInfo
+# 获取功能包下share目录或路径
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    robot_name_in_model = 'fishbot'
+    package_name = 'fishbot_description'
+    urdf_name = "fishbot_gazebo.urdf"
+
+    pkg_share = get_package_share_directory(f"{package_name}")
+    urdf_model_path = os.path.join(pkg_share, f'urdf/urdf/{urdf_name}')
+
+    # Start Gazebo server
+    start_gazebo_cmd = ExecuteProcess(
+        cmd=['gazebo', '--verbose','-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+        output='screen')
+        
+    # Launch the robot
+    spawn_entity_cmd = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=['-entity', robot_name_in_model,  '-file', urdf_model_path ], output='screen')
+
+
+    return LaunchDescription([start_gazebo_cmd,spawn_entity_cmd])
